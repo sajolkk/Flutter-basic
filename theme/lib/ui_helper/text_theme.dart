07e36aebbf2f4d5e3da1105ac2392915035fb5b7
sajolkk <sajolkar56@gmail.com>
2023-09-08 11:55:26 +0600

    learn theme

---
+++ theme/lib/ui_helper/text_theme.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+TextStyle mTextStyle1(){
+  return TextStyle(
+    fontSize: 40,
+    fontWeight: FontWeight.w900,
+  );
+}
+TextStyle mTextStyle2({Color textColor= Colors.black, FontWeight fontWeight= FontWeight.normal}){
+  return TextStyle(
+    fontSize: 30,
+    color: textColor,
+    fontWeight: fontWeight,
+  );
+}
